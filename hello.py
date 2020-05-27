6e62a7ffa272869a574520bc6b1f01282726e3ae
Michael LaCroix <michael.jp.lacroix@gmail.com>
2020-05-27 15:21:51 -0400

    more detailed display home page

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,3 +6,8 @@ app = Flask(__name__)
 
 def hello_world():
     return "SpaceX launch today!!!"
+
+@app.route("/about")
+
+def preds():
+    return render_template('home.html')
